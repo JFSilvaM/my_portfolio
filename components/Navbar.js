1e7c3dd53785f41062d7ab22e288fb538b022ced
João Silva <joaofrsima2000@gmail.com>
2022-04-12 19:27:20 +0200

    responsive nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,37 @@
 import { useTranslation } from "next-i18next";
+import { useState } from "react";
+import Hamburguer from "./icons/Hamburguer";
 
 export default function Navbar() {
   const { t } = useTranslation("common");
-  const pages = ["home", "about", "skillsTools"];
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const pages = ["home", "about", "skillsTools", "projects", "contact"];
 
   return (
-    <nav className="sticky top-0 bg-green-sections text-xl font-bold uppercase">
-      <ul className="flex flex-row justify-evenly py-6 md:container md:mx-auto">
+    <nav className="sticky top-0 bg-green-sections text-lg font-bold uppercase">
+      <section className="md:hidden">
+        <div
+          className="flex justify-center py-6"
+          onClick={() => setIsNavOpen((prev) => !prev)}
+        >
+          <Hamburguer />
+        </div>
+
+        <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+          <ul className="pb-5">
+            {pages.map((page) => (
+              <li key={`${page}`} className="flex justify-center py-1">
+                <a href={`#${page}`}>{t(`${page}`)}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+
+      <ul className="container mx-auto hidden justify-evenly py-6 md:flex">
         {pages.map((page) => (
           <li key={`${page}`}>
-            <a href={`#${page}`} className="a-underlined relative px-4 py-2">
+            <a href={`#${page}`} className="a-underlined relative py-2 px-4">
               {t(`${page}`)}
             </a>
           </li>
